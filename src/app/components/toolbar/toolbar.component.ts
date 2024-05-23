ec7ec5ffaeb7bbd882245f78d8709ad72a1fdbe8
Abner L. Carleto <abnercarleto@gmail.com>
2024-05-23 10:22:49 -0300

    Add Toolbar and Footer

---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [MatToolbar],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss',
+})
+export class ToolbarComponent {}
